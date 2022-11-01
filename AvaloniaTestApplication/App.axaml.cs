45f0013511cadc4c2bf78fefb08ac84b0874d657
Andrey Nasonov <37536541+Erapchu@users.noreply.github.com>
2022-11-01 14:07:29 +0700

    Trying DI with Avalonia

--- AvaloniaTestApplication/App.axaml.cs
+++ AvaloniaTestApplication/App.axaml.cs
@@ -3,11 +3,15 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using AvaloniaTestApplication.ViewModels;
 using AvaloniaTestApplication.Views;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace AvaloniaTestApplication
 {
     public partial class App : Application
     {
+        public IServiceProvider Services { get; private set; }
+
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
@@ -15,15 +19,30 @@ namespace AvaloniaTestApplication
 
         public override void OnFrameworkInitializationCompleted()
         {
+            ConfigureServiceProvider();
+
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
-                desktop.MainWindow = new MainWindow
-                {
-                    DataContext = new MainWindowViewModel(),
-                };
+                desktop.MainWindow = Services.GetService<MainWindow>();
             }
 
             base.OnFrameworkInitializationCompleted();
+        }
+
+        private void ConfigureServiceProvider()
+        {
+            var services = ConfigureServices();
+            Services = services.BuildServiceProvider();
+        }
+
+        private static IServiceCollection ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            services.AddTransient<MainWindow>();
+            services.AddTransient<MainWindowViewModel>();
+
+            return services;
         }
     }
 }
--- AvaloniaTestApplication/Views/MainWindow.axaml.cs
+++ AvaloniaTestApplication/Views/MainWindow.axaml.cs
@@ -1,12 +1,22 @@
 using Avalonia.Controls;
+using AvaloniaTestApplication.ViewModels;
 
 namespace AvaloniaTestApplication.Views
 {
     public partial class MainWindow : Window
     {
+        public MainWindowViewModel ViewModel { get; }
+
         public MainWindow()
         {
+
+        }
+
+        public MainWindow(MainWindowViewModel viewModel)
+        {
             InitializeComponent();
+            DataContext = viewModel;
+            ViewModel = viewModel;
         }
     }
 }
