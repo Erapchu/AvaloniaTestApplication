afa57b20f4ed8c2b93377d81cc134b1bbc7a6918
Andrey Nasonov <37536541+Erapchu@users.noreply.github.com>
2023-04-11 23:03:12 +0700

    Just test

--- AvaloniaTestApplication/ViewLocator.cs
+++ AvaloniaTestApplication/ViewLocator.cs
@@ -7,24 +7,30 @@ namespace AvaloniaTestApplication
 {
     public class ViewLocator : IDataTemplate
     {
-        public IControl Build(object data)
-        {
-            var name = data.GetType().FullName!.Replace("ViewModel", "View");
-            var type = Type.GetType(name);
+        //public IControl Build(object data)
+        //{
+        //    var name = data.GetType().FullName!.Replace("ViewModel", "View");
+        //    var type = Type.GetType(name);
 
-            if (type != null)
-            {
-                return (Control)Activator.CreateInstance(type)!;
-            }
-            else
-            {
-                return new TextBlock { Text = "Not Found: " + name };
-            }
-        }
+        //    if (type != null)
+        //    {
+        //        return (Control)Activator.CreateInstance(type)!;
+        //    }
+        //    else
+        //    {
+        //        return new TextBlock { Text = "Not Found: " + name };
+        //    }
+        //}
 
         public bool Match(object data)
         {
+            //Avalonia.Themes.Fluent.FluentTheme
             return data is ViewModelBase;
+        }
+
+        Control? ITemplate<object?, Control?>.Build(object? param)
+        {
+            throw new NotImplementedException();
         }
     }
 }
